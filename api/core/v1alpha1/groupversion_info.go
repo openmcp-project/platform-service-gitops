f662a2d40edd4873cf464ef85e59de5e49843755
Mykhailo Pechkurov <mykhailo.pechkurov@sap.com>
2026-07-21 12:15:36 +0200

    fix: override CRD group to gitops.open-control-plane.io

--- api/core/v1alpha1/groupversion_info.go
+++ api/core/v1alpha1/groupversion_info.go
@@ -1,22 +1,8 @@
-/*
-Copyright 2026.
+// SPDX-FileCopyrightText: 2026 SAP SE or an SAP affiliate company and Open Control Plane contributors
+// SPDX-License-Identifier: Apache-2.0
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-// Package v1alpha1 contains API Schema definitions for the core v1alpha1 API group.
 // +kubebuilder:object:generate=true
-// +groupName=core.openmcp.cloud
+// +groupName=gitops.open-control-plane.io
 package v1alpha1
 
 import (
@@ -25,20 +11,18 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
-var (
-	// SchemeGroupVersion is group version used to register these objects.
-	// This name is used by applyconfiguration generators (e.g. controller-gen).
-	SchemeGroupVersion = schema.GroupVersion{Group: "core.openmcp.cloud", Version: "v1alpha1"}
+// GroupVersion is group version used to register these objects.
+var GroupVersion = schema.GroupVersion{Group: "gitops.open-control-plane.io", Version: "v1alpha1"}
 
-	// GroupVersion is an alias for SchemeGroupVersion, for backward compatibility.
-	GroupVersion = SchemeGroupVersion
+// SchemeGroupVersion is an alias for GroupVersion for backward compatibility with
+// generated and scaffolded code that references it directly.
+var SchemeGroupVersion = GroupVersion
 
-	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
-	SchemeBuilder = runtime.NewSchemeBuilder(func(scheme *runtime.Scheme) error {
-		metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
-		return nil
-	})
+// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
+var SchemeBuilder = runtime.NewSchemeBuilder(func(scheme *runtime.Scheme) error {
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
+})
 
-	// AddToScheme adds the types in this group-version to the given scheme.
-	AddToScheme = SchemeBuilder.AddToScheme
-)
+// AddToScheme adds the types in this group-version to the given scheme.
+var AddToScheme = SchemeBuilder.AddToScheme
---
+++ api/core/v1alpha1/groupversion_info_test.go
@@ -0,0 +1,13 @@
+package v1alpha1_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/openmcp-project/platform-service-gitops/api/core/v1alpha1"
+)
+
+func TestGroupVersion_IsCorrect(t *testing.T) {
+	assert.Equal(t, "gitops.open-control-plane.io", v1alpha1.GroupVersion.Group)
+	assert.Equal(t, "v1alpha1", v1alpha1.GroupVersion.Version)
+}
