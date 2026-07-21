4871822d7bd1c0df7e1ed9ac9c597f288581b530
Mykhailo Pechkurov <mykhailo.pechkurov@sap.com>
2026-07-21 16:28:52 +0200

    fix: suppress deprecated scheme.Builder warning with nolint (staticcheck)
    Signed-off-by: Mykhailo Pechkurov <mykhailo.pechkurov@sap.com>

--- api/core/v1alpha1/groupversion_info.go
+++ api/core/v1alpha1/groupversion_info.go
@@ -19,7 +19,7 @@ var GroupVersion = schema.GroupVersion{Group: "gitops.open-control-plane.io", Ve
 var SchemeGroupVersion = GroupVersion
 
 // SchemeBuilder is used to add go types to the GroupVersionKind scheme.
-var SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+var SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion} //nolint:staticcheck
 
 // AddToScheme adds the types in this group-version to the given scheme.
 var AddToScheme = SchemeBuilder.AddToScheme
